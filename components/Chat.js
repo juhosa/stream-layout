ac3848e92c28cdbd0dcca65ab16b425c47f4e117
Juho Salli <juho.salli@gmail.com>
2021-10-30 13:30:24 +0300

    Chat (kinda) working, UI changes

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import { getClient, disconnect } from "../utils/tmi";
+import styles from "../styles/Chat.module.css";
+import { rehype } from "rehype";
+// import rehype from "rehype";
+import sanitize from "rehype-sanitize";
 
 const Chat = () => {
+  // const testMessages = [
+  //   { username: "juhosalli", message: "asd lol" },
+  //   { username: "viewer1", message: "True!" },
+  //   { username: "ALongNameBecauseWhyNot", message: ":D" },
+  //   {
+  //     username: "SuperCoder",
+  //     message:
+  //       "A longer message for testing purposes. I strongly disagree, oh boy.",
+  //   },
+  // ];
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const client = getClient();
 
     client.on("message", (channel, tags, message, self) => {
+      // console.log({ message });
+      // console.log({ channel });
+      // console.log({ tags });
       handleMessage(channel, tags, message, self);
     });
 
@@ -16,8 +33,56 @@ const Chat = () => {
     };
   }, []);
 
+  const handleEmotes = (message, { emotes = {} }) => {
+    let replaces = {};
+
+    for (let emoteid in emotes) {
+      const emote = emotes[emoteid];
+      const emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteid}/default/light/3.0`;
+      for (let indexes of emote) {
+        const split = indexes.split("-");
+        const startIndex = split[0];
+        const endIndex = split[1];
+        let thing = { startIndex, endIndex, emoteUrl };
+        replaces[startIndex] = thing;
+      }
+    }
+
+    // keys in descening order
+    const keys = Object.keys(replaces).sort((a, b) => Number(a) < Number(b));
+
+    for (const key of keys) {
+      const emote = replaces[key];
+      const start = message.substring(0, emote.startIndex);
+      const end = message.substring(emote.endIndex);
+      const image = `<img alt="emote" src="${emote.emoteUrl}">`;
+      message = start + image + end.substring(1);
+    }
+    return message;
+  };
+
   const handleMessage = (channel, tags, message, self) => {
-    // console.log(`${tags["display-name"]}: ${message}`);
+    message = handleEmotes(message, tags);
+
+    // Sanitize the message
+    // Let <img> and <marquee> to pass
+    message = rehype()
+      .data("settings", { fragment: true })
+      .use(sanitize, {
+        strip: ["script"],
+        protocols: {
+          src: ["https"],
+        },
+        tagNames: ["img", "marquee"],
+        attributes: {
+          img: ["src"],
+          "*": ["alt"],
+        },
+      })
+      .processSync(message)
+      .toString();
+    // console.log(message);
+
     const msg = {
       username: tags["display-name"],
       message: message,
@@ -33,11 +98,13 @@ const Chat = () => {
     });
   };
   return (
-    <div>
-      {messages.map((msg, i) => {
+    <div className={styles.chat_container}>
+      {messages.slice(-4).map((msg, i) => {
         return (
-          <p key={i}>
-            {msg.username}: {msg.message}
+          <p key={i} className={styles.message}>
+            <strong>{msg.username}:</strong>
+            {/* <span>{msg.message}</span> */}
+            <span dangerouslySetInnerHTML={{ __html: msg.message }} />
           </p>
         );
       })}
--- pages/main.js
+++ pages/main.js
@@ -1,24 +1,29 @@
-import Chat from '../components/Chat';
-import styles from '../styles/Main.module.css'
+import { useState } from "react";
+import Chat from "../components/Chat";
+import styles from "../styles/Main.module.css";
 
 const Main = () => {
+  const [title, setTitle] = useState("Building the stream layout!");
+  const [subtitle, setSubtitle] = useState("Some copyin' and some pastin'");
+
   return (
     <div className={styles.main}>
       <div className={styles.upperSection}>
-        <div className={styles.upperLeft}>upperLeft</div>
-        <div className={styles.upperRight}>upperRight</div>
+        <div className={styles.upperLeft}></div>
+        <div className={styles.upperRight}></div>
       </div>
 
       <div className={styles.bottomSection}>
         <div className={styles.bottomLeft}>
-          <h3>
-            Learn how CSS grid works!
-          </h3>
+          <h2 className={styles.title}>{title}</h2>
+          <span>{subtitle}</span>
         </div>
-        <div className={styles.bottomRight}><Chat /></div>
+        <div className={styles.bottomRight}>
+          <Chat />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Main;
